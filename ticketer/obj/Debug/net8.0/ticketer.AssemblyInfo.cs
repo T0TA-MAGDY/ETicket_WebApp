6a3d773875b4e794eb68cb45e491776aa0919f37
tota <youstena.ma@gmail.com>
2025-05-07 06:25:41 +0300

    added role intializer an user roles

--- ticketer/obj/Debug/net8.0/ticketer.AssemblyInfo.cs
+++ ticketer/obj/Debug/net8.0/ticketer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ticketer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d5fee4f37137f308f81008da42e837d0e7e83d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834867b073b2ceb6c740a16aacb1d2068f27c29d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ticketer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ticketer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
