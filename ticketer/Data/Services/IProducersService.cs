50604de94508a102a5f2df99993bce2f26c9be63
tota <youstena.ma@gmail.com>
2025-05-09 04:48:02 +0300

    actors/producers/cinemas controllres with services

---
+++ ticketer/Data/Services/IProducersService.cs
@@ -0,0 +1,12 @@
+﻿using ticketer.Data.Base;
+using ticketer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace eTickets.Data.Services
+{
+    public interface IProducersService : IEntityBaseRepository<Producer>
+    {
+    }
+}
