d6e6e88aecb415bd8dd558e9d22f814784f2b750
tota <youstena.ma@gmail.com>
2025-06-25 18:36:32 +0300

    fixed empty orders bug

--- ticketer/Data/Services/IOrdersService.cs
+++ ticketer/Data/Services/IOrdersService.cs
@@ -6,8 +6,6 @@ namespace ticketer.Data.Services
 {
     public interface IOrdersService
     {
-        Task StoreOrderAsync(List<int> timingIds, List<int> seatIds, string userId, string userEmailAddress);
-
         Task<List<TicketOrder>> GetOrdersByUserIdAndRoleAsync(string userId, string userRole);
     }
 }
